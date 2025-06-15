11518b2126d0a70f113c92dc8ed73f797f2be1a1
Fiolix <Fiolix@hotmail.com>
2025-06-15 22:03:01 +0200

    Update burger_menu.js

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -5,32 +5,31 @@ document.addEventListener("DOMContentLoaded", function () {
   navMenu.classList.add("slide-menu");
   navMenu.innerHTML = `
     <div style="margin-bottom: 2rem; font-size: 1.5rem;">
-      <a href="#home" title="Home"><img src="home-icon.png" alt="Home" style="width: 24px; height: 24px;" /></a>
+      <a href="index.html" title="Home"><img src="img/home_icon.png" alt="Home" style="width: 24px; height: 24px;" /></a>
     </div>
     <ul>
-      <li><a href="#news">News</a></li>
-      <li><a href="#agriturismo">Agriturismo La Cerra</a></li>
-      <li><a href="#sardinia">Sardinia</a></li>
+      <li><a href="news.html" class="depth-1">News</a></li>
+      <li><a href="agriturismo.html" class="depth-1">Agriturismo La Cerra</a></li>
+      <li><a href="sardinia.html" class="depth-1">Sardinia</a></li>
       <li class="toggleable">
-        <a href="#bouldering">Bouldering</a>
+        <a href="bouldering.html" class="depth-1">Bouldering</a>
         <ul>
           <li class="toggleable">
-            <a href="#lacerra">La Cerra</a>
+            <a href="la-cerra.html" class="depth-2">La Cerra</a>
             <ul>
-              <li><a href="#beispielsektor">Beispielsektor</a></li>
+              <li><a href="beispielsektor.html" class="depth-3">Beispielsektor</a></li>
             </ul>
           </li>
-          <li><a href="#gallura">Gallura</a></li>
+          <li><a href="gallura.html" class="depth-2">Gallura</a></li>
         </ul>
       </li>
-      <li><a href="#faq">FAQ</a></li>
+      <li><a href="faq.html" class="depth-1">FAQ</a></li>
     </ul>
 
     <div class="login-block">
-      <h3>Login</h3>
       <input type="text" id="user" name="user" placeholder="User" />
       <input type="password" id="password" name="password" placeholder="Password" />
-      <button type="button">Log In</button>
+      <button type="button">Login</button>
     </div>
 
     <div class="language-switcher">
@@ -47,6 +46,9 @@ document.addEventListener("DOMContentLoaded", function () {
       e.preventDefault();
       const parentLi = this.parentElement;
       parentLi.classList.toggle("open");
+      if (this.href) {
+        window.location.href = this.href;
+      }
     });
   });
 
@@ -58,6 +60,14 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!navMenu.contains(e.target) && !menuIcon.contains(e.target)) {
       navMenu.classList.remove("open");
     }
+  });
+
+  navMenu.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", function (e) {
+      if (this.classList.contains("depth-3")) {
+        navMenu.classList.remove("open");
+      }
+    });
   });
 });
 
