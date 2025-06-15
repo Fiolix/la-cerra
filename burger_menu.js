7f3d9387ca762c8c41769f6e9aadc4b271b50344
Fiolix <Fiolix@hotmail.com>
2025-06-15 20:35:39 +0200

    Update burger_menu.js

--- burger_menu.js
+++ burger_menu.js
@@ -33,12 +33,19 @@ document.addEventListener("DOMContentLoaded", function () {
       <button type="button">Log In</button>
     </div>
 
-    <div class="language-switcher">
+    <div class="language-switcher" style="display: flex; justify-content: center; gap: 0.5rem; padding: 1rem 0;">
       <img src="img/flag_en.png" alt="EN" title="English" />
       <img src="img/flag_it.png" alt="IT" title="Italiano" />
       <img src="img/flag_de.png" alt="DE" title="Deutsch" />
     </div>
   `;
+
+<script>
+  function setLanguage(lang) {
+    alert('Sprache wechseln zu: ' + lang);
+    // Hier kannst du später Logik zum tatsächlichen Sprachwechsel ergänzen
+  }
+</script>
 
   // Menü ganz oben einfügen
   document.body.insertBefore(navMenu, document.body.firstChild);
