d8df6b8d40f76b8c01b9394007e116cb8e08ee2e
Fiolix <Fiolix@hotmail.com>
2025-06-15 13:58:49 +0200

    Update burger_menu.js

--- burger_menu.js
+++ burger_menu.js
@@ -5,17 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
   navMenu.classList.add("slide-menu");
   navMenu.innerHTML = `
     <div style="margin-bottom: 2rem; font-size: 1.5rem;">
-      <a href="#home" title="Home">🏠</a>
+      <a href="#home" title="Home"><img src="home-icon.png" alt="Home" style="width: 24px; height: 24px;" /></a>
     </div>
     <ul>
       <li><a href="#news">News</a></li>
       <li><a href="#agriturismo">Agriturismo La Cerra</a></li>
       <li><a href="#sardinia">Sardinia</a></li>
-      <li>
+      <li class="toggleable">
         <a href="#bouldering">Bouldering</a>
         <ul>
-          <li><a href="#lacerra">La Cerra</a></li>
-          <li><a href="#beispielsektor">Beispielsektor</a></li>
+          <li class="toggleable">
+            <a href="#lacerra">La Cerra</a>
+            <ul>
+              <li><a href="#beispielsektor">Beispielsektor</a></li>
+            </ul>
+          </li>
           <li><a href="#gallura">Gallura</a></li>
         </ul>
       </li>
@@ -24,10 +28,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
     <div class="login-block">
       <h3>Login</h3>
-      <label for="user">User:</label>
-      <input type="text" id="user" name="user" />
-      <label for="password">Password:</label>
-      <input type="password" id="password" name="password" />
+      <input type="text" id="user" name="user" placeholder="User" />
+      <input type="password" id="password" name="password" placeholder="Password" />
       <button type="button">Log In</button>
     </div>
 
@@ -38,8 +40,19 @@ document.addEventListener("DOMContentLoaded", function () {
     </div>
   `;
 
-  document.body.appendChild(navMenu);
+  // Menü ganz oben einfügen
+  document.body.insertBefore(navMenu, document.body.firstChild);
 
+  // Dropdown-Menüs ein-/ausklappen
+  navMenu.querySelectorAll("li.toggleable > a").forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const parentLi = this.parentElement;
+      parentLi.classList.toggle("open");
+    });
+  });
+
+  // Burger-Menü anzeigen/verstecken
   menuIcon.addEventListener("click", function () {
     navMenu.classList.toggle("open");
   });
