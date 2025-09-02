a9272799ae3d3484baf6414b7b79c0f0628aee9c
Fiolix <Fiolix@hotmail.com>
2025-09-02 22:50:43 +0200

    Update auth_handler.js

--- js/auth_handler.js
+++ js/auth_handler.js
@@ -102,7 +102,7 @@ console.log("🔧 toggleStartLogin:", { isAuth: !!session?.user });
         } catch {}
         greet.textContent = `Ciao, ${name} –`;
       }
-    }
+    
 
   } else {
     // Ausgeloggt: Login-Form zeigen, Hello-Box verstecken
