827d45c801f0295effa71c18972de9d77bbc9c1a
Fiolix <Fiolix@hotmail.com>
2026-09-05 16:12:52 +0200

    Add interactive La Cerra sector map

--- js/content_loader.js
+++ js/content_loader.js
@@ -1,6 +1,6 @@
 // Zentrale Seitennavigation und Initialisierung dynamischer Inhalte.
 
-const ASSET_VERSION = "20260905-sectors-1";
+const ASSET_VERSION = "20260905-sectors-2";
 
 const PAGE_ALIASES = {
   start: "start.html",
